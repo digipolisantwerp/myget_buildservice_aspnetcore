453474eccd9730ba28c272cfc4acc5305c48e75b
JimmyHannon <jimmy.hannon@jh-tes.be>
2016-03-29 13:50:17 +0200

    removed build step

--- build.bat
+++ build.bat
@@ -1,30 +1,14 @@
-REM @echo Off
-set config=%1
-if "%config%" == "" (
-   set config=Release
-)
-
-set version=
-if not "%PackageVersion%" == "" (
-   set version=-Version %PackageVersion%
-)
-
-REM Start Build
-dnu build "src\SampleLib"
-REM Build done
-if not "%errorlevel%"=="0" goto failure
-
 REM Unit tests
 dnx  -p tests\SampleLib.Tests test
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
-REM mkdir Build
-REM call %nuget% pack "myget_buildservicetest_aspnetcore\SampleLib.csproj" -symbols -o Build -p Configuration=%config% %version%
-REM if not "%errorlevel%"=="0" goto failure
+mkdir Build
+dnu pack "src\SampleLib"
+if not "%errorlevel%"=="0" goto failure
 
 :success
-REM exit 0
+exit 0
 
 :failure
-REM exit -1
+exit -1
