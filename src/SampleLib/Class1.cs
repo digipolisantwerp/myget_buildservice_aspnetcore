293a4ebd64c7ed997a4edfab884845748ac62cf0
JimmyHannon <jimmy.hannon@jh-tes.be>
2016-03-29 14:10:03 +0200

    mod

--- src/SampleLib/Class1.cs
+++ src/SampleLib/Class1.cs
@@ -9,6 +9,8 @@ namespace SampleLib
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class Class1
     {
+
+
         public Class1()
         {
         }
